c7829a2caa3bcf1652ddd5dd0597b6dd7d236d83
Brian Chick <brian.chick@us.abb.com>
2017-03-28 16:05:27 -0600

    Added new projects and wired up dependencies.

---
+++ Domain.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Domain.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
