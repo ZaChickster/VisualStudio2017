3cd1abd0d59148b32490cba0c6f1bf844634631a
Brian Chick <brian.chick@us.abb.com>
2017-03-31 13:24:21 -0600

    Added ability to filter completed to-dos and highlight open todo rows in the table.  Changed actions to buttons.

--- Angular2/ClientApp/app/components/todo/todo.component.ts
+++ Angular2/ClientApp/app/components/todo/todo.component.ts
@@ -5,13 +5,16 @@ import { ToDo } from '../../models/interfaces';
 
 @Component({
     selector: 'todo',
-    templateUrl: './todo.component.html'
+    templateUrl: './todo.component.html',
+    styleUrls: ['./todo.component.css']
 })
 
 export class ToDoComponent {
     ajax: Http;
 
     public model: ToDo[];
+    public filtered: ToDo[];
+    public showCompleted = true;
 
     constructor(http: Http, route: ActivatedRoute) {
         var me = this;
@@ -25,8 +28,15 @@ export class ToDoComponent {
 
         this.ajax.get(url).subscribe(result => {
             this.model = result.json() as ToDo[];
+            this.toggleCompleted();
         });
 
         return false;
+    }
+
+    toggleCompleted() {
+        this.filtered = [];
+        this.filtered = this.model.filter(todo => todo.completed === this.showCompleted || todo.completed === false);
+        this.showCompleted = !this.showCompleted;
     }
 }
