bac6b34c0d310f2cba3e467d85d46dcb29fac44e
Brian Chick <brian.chick@us.abb.com>
2017-05-03 14:32:40 -0600

    added a simple custom component.

--- VueJs/src/components/Hello.vue
+++ VueJs/src/components/Hello.vue
@@ -1,4 +1,4 @@
-<template>
+﻿<template>
   <div class="hello">
     <h1>{{ msg }}</h1>
     <h2>Essential Links</h2>
@@ -7,7 +7,6 @@
       <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
       <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-      <br>
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
     </ul>
     <h2>Ecosystem</h2>
---
+++ VueJs/src/components/Sandbox.vue
@@ -0,0 +1,24 @@
+﻿<template>
+  <div class="sandbox" v-bind:title="bound">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sandbox',
+  data () {
+    return {
+      message: 'Welcome to the Sandbox control.',
+      bound: 'Generated at ' + new Date()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: bold;
+}
+</style>
