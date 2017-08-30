78f9f806003116aac1ad495ef0f619aae21cf751
Brian Chick <btctech@gmail.com>
2017-08-30 15:38:07 -0600

    Able to listen to the route value changing to trigger loading of the grid.

--- AngularCli/src/app/products/products.resolver.ts
+++ AngularCli/src/app/products/products.resolver.ts
@@ -1,6 +1,8 @@
 import { Injectable }       from '@angular/core';
 import {
   Resolve,
+  Router,
+  NavigationEnd,
   ActivatedRouteSnapshot
 }                           from '@angular/router';
 import { ProductsSandbox }  from './products.sandbox';
@@ -10,7 +12,7 @@ export class PageResolver implements Resolve<any> {
 
   private pageSubscription;
 
-  constructor(public productsSandbox: ProductsSandbox) {}
+  constructor(public productsSandbox: ProductsSandbox, public routes: Router) {}
 
   /**
    * Triggered when application hits product details route.
@@ -22,12 +24,23 @@ export class PageResolver implements Resolve<any> {
     if (this.pageSubscription) return;
 
     this.pageSubscription = this.productsSandbox.model$.subscribe(model => {
-      if (!model || model.currentPage < 0) {
-        this.productsSandbox.loadProducts(parseInt(route.params.page) - 1);
+      if (!model || model.currentPage < 0) {        
         return;
       }
 
       this.productsSandbox.loadSuccess(model);
+    });
+
+    this.routes.events.subscribe(args => {
+      if (args instanceof NavigationEnd) {
+        var routeEnd : string = args.urlAfterRedirects.substring(args.urlAfterRedirects.lastIndexOf('/') + 1);
+        var parsed = parseInt(routeEnd);
+        var requested : number = isNaN(parsed) ? 0 : parsed;
+
+        if (this.productsSandbox.currentPage !== requested) {
+          this.productsSandbox.loadProducts(requested - 1);
+        }  
+      }            
     });
   }
 }
